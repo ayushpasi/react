1cb0cf47a90e228796d370fedcf9d2b34988fb9d
ayushpasi <pasiayush999@gmail.com>
2024-05-26 12:26:01 +0530

    4 ways to use css in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./UserProfile.css";
+import style from "./UserProfile.module.css";
+const App = () => {
+  //inline style for container
+  const styleContainer = { textAlign: "center", margin: "20px" };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styleContainer}>
+      <img
+        src="https://cdn.pixabay.com/photo/2023/01/03/16/00/dog-7694676_960_720.jpg"
+        alt="dog"
+        //appling inline style directly
+        style={{
+          height: "150px",
+          weight: "150px",
+          borderRadius: "50%",
+          objectFit: "cover",
+        }}
+      />
+      <h2 className="name">Optimized</h2>
+
+      <p className="bio">Fullstack web developer</p>
+      <ul className={style.hobbies}>
+        <li className={style.hobby}>Coding</li>
+        <li className={style.hobby}>Hiking</li>
+        <li className={style.hobby}>Travelling</li>
+      </ul>
     </div>
   );
-}
-
+};
 export default App;
