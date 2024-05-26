f722d6f239481917c39bac3a063594c7bc0733ff
ayushpasi <pasiayush999@gmail.com>
2024-05-26 21:59:36 +0530

    counter added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,11 +2,50 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
   return (
-    <div style={{ textAlign: "center" }}>
+    <div
+      style={{
+        textAlign: "center",
+        padding: "10px",
+        margin: "20px",
+        color: "red",
+      }}
+    >
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button
+        onClick={() => {
+          increment();
+        }}
+        style={{ margin: "0 5px" }}
+      >
+        increment
+      </button>
+
+      <button
+        onClick={() => {
+          decrement();
+        }}
+        style={{ margin: "0 5px" }}
+      >
+        decrement
+      </button>
+      <button
+        onClick={() => {
+          reset();
+        }}
+        style={{ margin: "0 5px" }}
+      >
+        reset
+      </button>
     </div>
   );
 };
