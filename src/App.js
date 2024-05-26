7997dc721a2c66a6e6e5fd69020925e07cb321c3
ayushpasi <pasiayush999@gmail.com>
2024-05-26 17:45:21 +0530

    creating demo component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import "./UserProfile.css";
-import style from "./UserProfile.module.css";
+import UserProfile from "./components/UserProfile";
 const App = () => {
-  //inline style for container
-  const styleContainer = { textAlign: "center", margin: "20px" };
   return (
-    <div style={styleContainer}>
-      <img
-        src="https://cdn.pixabay.com/photo/2023/01/03/16/00/dog-7694676_960_720.jpg"
-        alt="dog"
-        //appling inline style directly
-        style={{
-          height: "150px",
-          weight: "150px",
-          borderRadius: "50%",
-          objectFit: "cover",
-        }}
-      />
-      <h2 className="name">Optimized</h2>
-
-      <p className="bio">Fullstack web developer</p>
-      <ul className={style.hobbies}>
-        <li className={style.hobby}>Coding</li>
-        <li className={style.hobby}>Hiking</li>
-        <li className={style.hobby}>Travelling</li>
-      </ul>
-      <div className="alert alert-primary" role="alert">
-        A simple primary alert—check it out!
-      </div>
-    </div>
+    //rendring the component
+    <>
+      <UserProfile />
+    </>
   );
 };
 export default App;
---
+++ src/components/UserProfile.js
@@ -0,0 +1,25 @@
+const UserProfile = () => {
+  return (
+    <div style={{ border: "1px solid #ccc", padding: "20px", margin: "20px" }}>
+      <h2>User Profile</h2>
+      <p>
+        <b>Name</b>:Emannuel
+      </p>
+      <p>
+        <b>Email</b>:Emannuel@gmail.com
+      </p>
+      <p>
+        <b>Location</b>:Kumashi gana
+      </p>
+      <p>
+        <b>About</b>:The standard chunk of Lorem Ipsum used since the 1500s is
+        reproduced below for those interested. Sections 1.10.32 and 1.10.33 from
+        "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their
+        exact original form, accompanied by English versions from the 1914
+        translation by H. Rackham.
+      </p>
+    </div>
+  );
+};
+
+export default UserProfile;
