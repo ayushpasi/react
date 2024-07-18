84a7a97973c31d360e798471ac016ae70c649056
ayushpasi <pasiayush999@gmail.com>
2024-07-18 23:37:02 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import StudentRegistration from "./components/StudentRegistration";
+import Home from "./components/Home";
+import About from "./components/About";
+import Admin from "./components/Admin";
+import Contact from "./components/Contact";
+import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     //rendring the component
     <>
-      <StudentRegistration />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route
+            path="*"
+            element={
+              <h1 style={{ textAlign: "center", color: "green" }}>
+                404 Not Found
+              </h1>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed felis
+        finibus lacus maximus imperdiet. Pellentesque habitant morbi tristique
+        senectus et netus et malesuada fames ac turpis egestas. Etiam eu magna
+        dolor. Etiam eget blandit metus. Mauris vehicula tellus ac orci porta,
+        ut mattis elit sagittis. Nam metus lectus, aliquam ac quam vitae,
+        molestie iaculis orci. Vivamus ultrices mauris nec neque consequat,
+        porttitor condimentum odio hendrerit. Aenean quis lorem a justo
+        tincidunt scelerisque id sed turpis. Ut blandit lorem nec aliquet
+        blandit. Nullam efficitur tellus eget magna venenatis, vitae scelerisque
+        quam fermentum. Sed a felis molestie, gravida nibh ac, dignissim purus.
+        Integer libero sem, fermentum vitae mi in, pretium auctor est. Nullam
+        maximus sem et viverra sagittis. Donec egestas leo commodo arcu faucibus
+        lobortis. Vestibulum sit amet mauris ac orci sodales aliquam venenatis
+        at quam. Ut ut bibendum metus, et mollis ex.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
